c512cae442d5eaf271a600ad561710ea3490607b
Radukux <radu937@yahoo.com>
2020-02-21 18:12:58 +0200

    data transformed from excel to csv

---
+++ server/enums/premierleagueenum.py
@@ -0,0 +1,24 @@
+import enum
+
+
+class PremierLeague(enum.Enum):
+    arsenal_info = 'Arsenal Results.xlsx'
+    aston_villa_info = 'Aston Villa Results.xlsx'
+    bournemouth_info = 'Bournemouth Results.xlsx'
+    brighton_info = 'Brighton & Hove Albion Results.xlsx'
+    burnley_info = 'Burnley Results.xlsx'
+    chelsea_info = 'Chelsea Results.xlsx'
+    crystal_palace_info = 'Crystal Palace Results.xlsx'
+    everton_info = 'Everton Results.xlsx'
+    leicester_info = 'Leicester City Results.xlsx'
+    liverpool_info = 'Liverpool Results.xlsx'
+    manchester_city_info = 'Manchester City Results.xlsx'
+    manchester_utd_info = 'Manchester United Results.xlsx'
+    newcastle_info = 'Newcastle United Results.xlsx'
+    norwich_info = 'Norwich Results.xlsx'
+    sheffield_info = 'Sheffield United Results.xlsx'
+    southampton_info = 'Southampton Results.xlsx'
+    tottenham_info = 'Tottenham Hotspur Results.xlsx'
+    watford_info = 'Watford Results.xlsx'
+    west_ham_info = 'West Ham United Results.xlsx'
+    wolves_info = 'Wolverhampton Wanderers Results.xlsx'
--- server/readexcelwritecsv.py
+++ server/readexcelwritecsv.py
@@ -1,12 +1,45 @@
 import pandas as pd
+import csv
+import re
+import io
+import server.enums.premierleagueenum as ple
+
 
 class ReadExcelWriteCsv:
 
-    def liverpool_info(self):
-        liverpool_data = 'excel_data/Liverpool Results.xlsx'
+    def __write_csv_info(self, from_excel, to_csv):
+        liverpool_data = 'excel_data/' + from_excel
         xl = pd.ExcelFile(liverpool_data)
-        df1 = xl.parse('Sheet2')
+        df1 = xl.parse('Sheet1')
+
         print(df1)
 
+        with io.open('csv_data/' + to_csv, 'w', encoding="utf-8") as f:
+            writer = csv.writer(f)
+
+            for index, row in df1.iterrows():
+                row['Score'] = re.sub(
+                    " \\([0-9]:[0-9]\\)| \\([0-9]:[0-9], [0-9]:[0-9]\\)| \\([0-9]:[0-9], [0-9]:[0-9], [0-9]:[0-9]\\)| "
+                    "pso| aet",
+                    "", row['Score'])
+                vector = row['Score'].split(':')
+                team_goals = vector[0]
+                oponent_goals = vector[1]
+                if team_goals > oponent_goals:
+                    row['Result'] = 'v'
+                elif team_goals == oponent_goals:
+                    row['Result'] = 'e'
+                else:
+                    row['Result'] = 'd'
+                writer.writerow(row)
+
+        print("Premier League loaded sucessfully into csv files")
+
+    def load_premier_league(self):
+        for name in ple.PremierLeague:
+            self.__write_csv_info(name.value, name.name)
+
+
 repo = ReadExcelWriteCsv()
-repo.liverpool_info()
+repo.load_premier_league()
+
